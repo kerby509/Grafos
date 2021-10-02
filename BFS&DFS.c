1afad6fb64e2f25af3340efb698e8ccbbeffd057
kerby <lovincekerby99@gmail.com>
2021-10-02 15:45:34 -0300

    commit

--- BFS&DFS.c
+++ BFS&DFS.c
@@ -4,3 +4,15 @@
 
 int a[20][20], q[20], visitado[20];
 int i, j,n, f=0, r=-1;
+
+void bfs(int v){
+  for (i=0; i<n; i++){
+    if(a[v][i] && !visitado[i]){
+      q[++r]=i;
+    }
+    if(f<=r){
+      //visitando[q][f]=1;
+      bfs(q[++f]);
+    }
+  }
+}
