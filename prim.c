4a9bf5c157ab6090720b19429d0f4f8c71460f36
kerby <lovincekerby99@gmail.com>
2021-10-11 13:32:35 -0300

    commit

--- prim.c
+++ prim.c
@@ -5,3 +5,12 @@
 void printPrim(int **matTab, int n, int visitado){
     printf("\tver\t|\tvis\t|\tval\t|\tant\t|\n");
     printf("----------------+---------------+---------------+---------------|\n");
+        for(int i = 0; i < n; i++){
+        for(int j = 0; j < 4; j++){
+            if(matTab[i][j] == INT_MAX){
+                printf("\tINF\t|");
+            }
+            else{
+                printf("\t%d\t|", matTab[i][j]);
+            }
+        }
