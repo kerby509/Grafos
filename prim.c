cf2025b6ce336b84c6702fd504080f71da014daa
kerby <lovincekerby99@gmail.com>
2021-10-11 13:27:09 -0300

    commit

---
+++ prim.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+// aqui estou printando os valores
+void printPrim(int **matTab, int n, int visitado){
+    printf("\tver\t|\tvis\t|\tval\t|\tant\t|\n");
+    printf("----------------+---------------+---------------+---------------|\n");
